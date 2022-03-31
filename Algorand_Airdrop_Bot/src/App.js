734d1a828ac6983a1790c2f9c92e880d47a1c1dd
Kingsley Chuwkuemeka <64647601+ChukwuemekaKingsley@users.noreply.github.com>
2022-03-31 23:09:57 +0100

    lets go

---
+++ Algorand_Airdrop_Bot/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import CreateAct from "./components/CreateAct";
+import CreateForm from "./components/CreateForm";
+
+function App() {
+  return (
+    <>
+      <CreateAct />
+    </>
+  );
+}
+
+export default App;
